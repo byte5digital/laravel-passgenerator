1c95ce5b6f6237cc3796bdc5a72f219f529190a5
Laurent Goerres <lgoerres@byte5.de>
2025-08-19 10:02:21 +0200

    feat(config_disk): add optional seperate config disk

--- config/passgenerator.php
+++ config/passgenerator.php
@@ -1,8 +1,10 @@
 <?php
 
 return [
+    'config_disk' => env('PASSGENERATOR_CONFIG_DISK', 'local'), // The disk to use for storing the pass configuration files and certificates
+
     'certificate_store_path' => env('CERTIFICATE_PATH', ''), // The path to the certificate store (a valid  PKCS#12 file)
-    'certificate_store_password' => env('CERTIFICATE_PASS', ''), // The password to unlo
+    'certificate_store_password' => env('CERTIFICATE_PASS', ''), // The password to unlock the certificate store
     'wwdr_certificate_path' => env('WWDR_CERTIFICATE', ''), // Get from here https://www.apple.com/certificateauthority/ and export to PEM
 
     'storage_disk' => env('PASSGENERATOR_STORAGE_DISK', 'local'), // The disk to use for storing the pass files
